3508197fc1ee87490a0a88dfc6a99a50443c9e5b
Dave Grantham <dwh@linuxprogrammer.org>
2024-04-16 02:37:31 -0600

    fix blocks interface
    Signed-off-by: Dave Grantham <dwh@linuxprogrammer.org>

--- src/storage/blocks.rs
+++ src/storage/blocks.rs
@@ -1,12 +1,15 @@
 // SPDX-License-Identifier: FSL-1.1
 use multicid::Cid;
-use crate::Value;
 
 /// Block storage trait for getting and putting content address data
 pub trait Blocks {
+    /// The error type returned in put
+    type Error;
+
     /// Try to get a block from it's content address
-    fn get(&self, cid: &Cid) -> Option<Value>;
+    fn get(&self, cid: &Cid) -> Option<Vec<u8>>;
 
     /// Try to put a block and get back its content address
-    fn put(&mut self, data: &Value) -> Option<Cid>;
+    fn put<F: FnMut(&dyn AsRef<[u8]>) -> Result<Cid, Self::Error>>(
+        &mut self, data: &dyn AsRef<[u8]>, gen_cid: F) -> Result<Cid, Self::Error>;
 }
